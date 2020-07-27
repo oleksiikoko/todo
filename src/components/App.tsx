d36652103a50509be439767c5364a1dfa1208b6b
Oleksii Kovalchuk <oleksiij.ko@gmail.com>
2020-07-27 19:00:35 +0300

    Initial commit

---
+++ src/components/App.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import "../styles/index.scss";
+
+class App extends React.PureComponent {
+  render() {
+    return (
+      <div>
+        <h1>Hello World!</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
