bc5bfd01bbe6f253116ea8d0bd950b5e2c61b92f
Oleksii Kovalchuk <oleksiij.ko@gmail.com>
2020-08-01 19:18:03 +0300

    ToDoLocalStorageObserver tests

---
+++ src/__tests__/utils/ToDoLocalStorageObserver.test.ts
@@ -0,0 +1,42 @@
+import { ToDoLocalStorageObserver } from "../../utils";
+
+describe("ToDoLocalStorageObserver", () => {
+  const toDoLocalStorageObserver: ToDoLocalStorageObserver = new ToDoLocalStorageObserver();
+  const mockSubscriber = jest.fn();
+  toDoLocalStorageObserver.subscribe(mockSubscriber);
+
+  it("get list()", () => {
+    expect(toDoLocalStorageObserver.list).toEqual([]);
+  });
+
+  it("addTask()", () => {
+    toDoLocalStorageObserver.addTask({ id: "id1", name: "name1", done: false });
+
+    expect(mockSubscriber).toHaveBeenCalledTimes(1);
+  });
+
+  it("changeDone()", () => {
+    toDoLocalStorageObserver.changeDone("id1");
+
+    expect(mockSubscriber).toHaveBeenCalledTimes(2);
+  });
+
+  it("deleteTask()", () => {
+    toDoLocalStorageObserver.deleteTask("id1");
+
+    expect(mockSubscriber).toHaveBeenCalledTimes(3);
+  });
+
+  it("searchTasks()", () => {
+    toDoLocalStorageObserver.searchTasks("name1");
+
+    expect(mockSubscriber).toHaveBeenCalledTimes(4);
+  });
+
+  it("unsubscribe()", () => {
+    toDoLocalStorageObserver.unsubscribe(mockSubscriber);
+    toDoLocalStorageObserver.addTask({ id: "id1", name: "name1", done: false });
+
+    expect(mockSubscriber).toHaveBeenCalledTimes(4);
+  });
+});
