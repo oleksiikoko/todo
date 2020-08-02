6317784f6574a6b1d929d5817830b33875018e84
Oleksii Kovalchuk <oleksiij.ko@gmail.com>
2020-08-02 22:20:41 +0300

    Todo page tests

--- src/__tests__/pages/Todo.test.tsx
+++ src/__tests__/pages/Todo.test.tsx
@@ -14,54 +14,112 @@ describe("Todo page", () => {
     themeModeObserver: new ThemeModeLocalStorageObserver(),
   };
 
-  beforeEach(() => {
-    render(<Todo {...props} />);
-  });
+  describe("render", () => {
+    beforeEach(() => {
+      render(<Todo {...props} />);
+    });
 
-  it("render", () => {
-    expect(screen.queryByTestId("todo")).toHaveClass("todo light-mode");
-    expect(screen.queryByTestId("todo__container")).toHaveClass(
-      "todo__container"
-    );
-    expect(screen.queryByTestId("theme-changer")).toBeTruthy();
-    expect(
-      screen.queryByPlaceholderText("Enter task name for search...")
-    ).toBeTruthy();
-    expect(screen.queryByTestId("task-list")).toBeTruthy();
-    expect(screen.queryByTestId("add-task")).toBeTruthy();
+    it("container", () => {
+      expect(screen.queryByTestId("todo")).toHaveClass("todo light-mode");
+      expect(screen.queryByTestId("todo__container")).toHaveClass(
+        "todo__container"
+      );
+    });
+
+    it("components", () => {
+      expect(screen.queryByTestId("theme-changer")).toBeTruthy();
+      expect(
+        screen.queryByPlaceholderText("Enter task name for search...")
+      ).toBeTruthy();
+      expect(screen.queryByTestId("task-list")).toBeTruthy();
+      expect(screen.queryByTestId("add-task")).toBeTruthy();
+    });
   });
 
   describe("actions", () => {
+    beforeEach(() => {
+      render(<Todo {...props} />);
+    });
     it("change theme", () => {
       act(() => {
         fireEvent.click(screen.queryByText("Dark")!);
       });
       expect(screen.queryByTestId("todo")).toHaveClass("todo dark-mode");
     });
-  });
 
-  it("add task", () => {
-    act(() => {
-      fireEvent.change(
-        screen.queryByPlaceholderText(
-          'Enter task and press "Add task" button'
-        )!,
-        { target: { value: "task1" } }
-      );
+    it("add task", () => {
+      [...Array(5).keys()].map((item) => {
+        act(() => {
+          fireEvent.change(
+            screen.queryByPlaceholderText(
+              'Enter task and press "Add task" button'
+            )!,
+            { target: { value: item.toString() } }
+          );
+        });
+        act(() => {
+          fireEvent.click(screen.queryByTestId("add-task__button")!);
+        });
+      });
+
+      expect(props.toDoObserver.list).toHaveLength(5);
+      expect(screen.queryAllByTestId("task-item")).toHaveLength(5);
     });
-    act(() => {
-      fireEvent.click(screen.queryByTestId("add-task__button")!);
+
+    it("search task", () => {
+      act(() => {
+        fireEvent.change(
+          screen.queryByPlaceholderText("Enter task name for search...")!,
+          {
+            target: { value: "1" },
+          }
+        );
+      });
+
+      expect(props.toDoObserver.list).toHaveLength(1);
+      expect(screen.queryAllByTestId("task-item")).toHaveLength(1);
+      //restore searchline=====================================================
+      act(() => {
+        fireEvent.change(
+          screen.queryByPlaceholderText("Enter task name for search...")!,
+          {
+            target: { value: "" },
+          }
+        );
+      });
+
+      //=========================================================================
+    });
+    it("done task", () => {
+      act(() => {
+        fireEvent.click(screen.getAllByTestId("task-item")[0]!);
+      });
+
+      expect(screen.queryAllByText("Delete")).toHaveLength(1);
     });
 
-    expect(props.toDoObserver.list).toHaveLength(1);
-    expect(screen.queryAllByTestId("task-item")).toHaveLength(1);
-  });
+    it("decline delete task", () => {
+      act(() => {
+        fireEvent.click(screen.queryAllByText("Delete")[0]!);
+      });
+      act(() => {
+        fireEvent.click(screen.queryByText("Cancel")!);
+      });
 
-  //   it("search task", () => {
-  //       act(() => {
-  //         fireEvent.change(screen.queryByPlaceholderText("Enter task name for search...")!);
-  //       });
-  //       expect(screen.queryByTestId("todo")).toHaveClass("todo dark-mode");
-  //     });
-  //   });
+      expect(props.toDoObserver.list).toHaveLength(5);
+      expect(screen.queryAllByTestId("task-item")).toHaveLength(5);
+    });
+
+    it("allow delete task", () => {
+      act(() => {
+        fireEvent.click(screen.queryAllByText("Delete")[0]!);
+      });
+      act(() => {
+        fireEvent.click(screen.queryByText("Confirm")!);
+      });
+
+      expect(props.toDoObserver.list).toHaveLength(4);
+      expect(screen.queryAllByTestId("task-item")).toHaveLength(4);
+    });
+  });
 });
